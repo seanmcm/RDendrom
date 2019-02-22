ff348f5c05ea3402cbcb1417afa8115ba64b51d9
McMahon <sean.mcmahon73@gmail.com>
2019-02-22 09:44:53 -0500

    fixing vignette, naming and loading from github

---
+++ vignettes/Rdendrom_vignette.R
@@ -0,0 +1,10 @@
+## ----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----echo=TRUE, eval=TRUE------------------------------------------------
+library(Rdendrom)
+data(INPUT_data)
+
