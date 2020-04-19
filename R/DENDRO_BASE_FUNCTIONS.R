72f276dd785bb8a8aa559e742b2dead57b206bfb
McMahon <sean.mcmahon73@gmail.com>
2020-04-19 11:18:35 -0400

    Update DENDRO_BASE_FUNCTIONS.R
    another bug fix

--- R/DENDRO_BASE_FUNCTIONS.R
+++ R/DENDRO_BASE_FUNCTIONS.R
@@ -37,7 +37,7 @@ get.optimized.dendro <- function(INPUT.data,
   TREE.ID.YR <- paste(as.character(INPUT.data$SITE), as.character(INPUT.data$TREE_ID),
     as.character(INPUT.data$YEAR), sep = "_")
   Dendro.split <- vector("list", length = length(unique(TREE.ID.YR)))
-  ind.dendro <- split(INPUT.data, f = INPUT.data$TREE.ID.YR, drop = TRUE)
+  ind.dendro <- split(INPUT.data, f = TREE.ID.YR, drop = TRUE)
 
   n.obs <- length(ind.dendro)
 
